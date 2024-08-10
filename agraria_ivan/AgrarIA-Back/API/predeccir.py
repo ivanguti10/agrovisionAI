99bd182f0bbc43cfe03e3c7b3d92f88bb6a58636
ivanguti10 <83346432+ivanguti10@users.noreply.github.com>
2024-08-10 18:48:40 +0200

    Switched to TensorFlow Lite and prepared for deployment

--- agraria_ivan/AgrarIA-Back/API/predeccir.py
+++ agraria_ivan/AgrarIA-Back/API/predeccir.py
@@ -13,20 +13,15 @@ from pathlib import Path
 import os
 import matplotlib.pyplot as plt # type: ignore
 import numpy as np # type: ignore
-import tensorflow as tf # type: ignore
-from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
-from tensorflow.keras.applications import ResNet50 # type: ignore
-from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D # type: ignore
-from tensorflow.keras.optimizers import Adam # type: ignore
+
 import datetime
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard # type: ignore
 import cv2 # type: ignore
 import json
-from tensorflow.keras.regularizers import l2 # type: ignore
-from tensorflow.keras.applications.resnet50 import preprocess_input # type: ignore
+
 
 import urllib.parse
+import tflite_runtime.interpreter as tflite
+
 
 
 app = Flask(__name__, static_folder='../../AgrarIA-Front/dist/agrar-ia')
@@ -83,33 +78,21 @@ root_path = Path("C:\\Users\\ivan2\\OneDrive\\Escritorio\\newplantvillage\\newpl
 root_path_str = str(root_path)
 classes_train = ['Apple scab', 'Apple Black Rot', 'Manzano Roya del manzano y del cedro', 'Manzano saludable', 'Arándano saludable', 'Cereza (incluyendo ácida) Oidio', 'Cereza (incluyendo ácida) saludable', 'Vid Podredumbre negra', 'Vid Esca (Sarampión negro)', 'Vid Tizón de la hoja (Mancha de la hoja de Isariopsis)', 'Vid saludable', 'Naranja Huanglongbing (Greening de los cítricos)', 'Melocotón Mancha bacteriana', 'Melocotón saludable', 'Pimiento, morrón Mancha bacteriana', 'Pimiento, morrón saludable', 'Patata Tizón temprano', 'Patata Tizón tardío', 'Patata saludable', 'Frambuesa saludable', 'Soja saludable', 'Calabaza Oidio', 'Fresa Chamusco de la hoja', 'Fresa saludable', 'Tomate Mancha bacteriana', 'Tomate Tizón temprano', 'Tomate Tizón tardío', 'Tomate Moho de la hoja', 'Tomate Mancha foliar de Septoria', 'Tomate Araña roja (Ácaro de dos manchas)', 'Tomate Mancha diana', 'Tomate Virus del rizado amarillo de la hoja del tomate', 'Tomate Virus del mosaico del tomate', 'Tomate saludable']
 
-# Cargar el modelo base ResNet50 preentrenado en ImageNet sin la parte superior
-base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(200, 200, 3))
-base_model.trainable = True
 
-# Crear un nuevo modelo
-model = Sequential([
-    base_model,
-    GlobalAveragePooling2D(),
-    Dense(512, activation='relu'),
-    Dropout(0.5),
-    Dense(256, activation='relu'),
-    Dropout(0.5),
-    Dense(128, activation='relu'),
-    Dropout(0.5),
-    Dense(len(classes_train), activation='softmax')  # Asegúrate de que el número de clases es correcto
-])
 
-# Compilar el modelo
-model.compile(optimizer=Adam(learning_rate=0.00001), loss='categorical_crossentropy', metrics=['accuracy'])
-
-model_path = 'C:\\Users\\ivan2\\OneDrive\\Escritorio\\best_model.h5'
+model_path = 'C:\\Users\\ivan2\\OneDrive\\Escritorio\\converted_model.tflite'
 
 if not os.path.exists(model_path):
     raise FileNotFoundError(f"El archivo de modelo en la ruta {model_path} no existe.")
 
 try:
-    model = tf.keras.models.load_model(model_path)
+    interpreter = tflite.Interpreter(model_path)
+    interpreter.allocate_tensors()
+
+    # Obtener detalles del tensor de entrada y salida
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
     print("Modelo cargado exitosamente")
 except Exception as e:
     print(f"Error al cargar el modelo: {e}")
@@ -117,10 +100,6 @@ except Exception as e:
 print(classes_train)  # Muestra los nombres de todas las clases
 #model.summary()  # Verificar el resumen del modelo
 
-
-
-import tensorflow as tf # type: ignore
-print(tf.__version__)
 
 @app.route('/plagas', methods=['GET'])
 def get_plagas():
@@ -148,14 +127,17 @@ def predict():
 
         if img is not None:
             img_resize = cv2.resize(img, (200, 200))
-            dataset = np.array([img_resize]) / 255.0
+            
+            # Aquí es donde debes normalizar la imagen
+            input_data = np.array([img_resize], dtype=np.float32) / 255.0  # Normaliza a [0, 1]
 
-            # Realizar la predicción
-            predictions = model.predict(dataset)
-            confidence = np.max(predictions, axis=-1)[0] * 100
-            result = np.argmax(predictions, axis=-1)
+            interpreter.set_tensor(input_details[0]['index'], input_data)
+            interpreter.invoke()
+            output_data = interpreter.get_tensor(output_details[0]['index'])
 
-            # Construcción del resultado en formato JSON
+            confidence = np.max(output_data, axis=-1)[0] * 100
+            result = np.argmax(output_data, axis=-1)
+
             predicted_class = classes_train[result[0]]
             final_json = {
                 "filename": file.filename,
